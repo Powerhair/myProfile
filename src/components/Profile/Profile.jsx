4795f62536b5bf7868ab012f5c241f7ce89a433d
LubovPopova <gohre94@gmail.com>
2023-08-14 13:42:40 +0300

    ссылки в контактах

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,7 +1,23 @@
 import "./Profile.scss";
 import profile from "../../images/profile.png";
+import { useState } from "react";
 
 function Profile({ isTheme }) {
+  const [isCopied, setIsCopied] = useState(false);
+
+  const copyEmail = () => {
+    navigator.clipboard
+      .writeText("gohre94@gmail.com")
+      .then(() => {
+        setIsCopied(true);
+        setTimeout(() => {
+          setIsCopied(false);
+        }, 2000);
+      })
+      .catch((error) => {
+        console.error("Не удалось скопировать адрес электронной почты", error);
+      });
+  };
   return (
     <div className={`profile ${isTheme ? "" : "profile-dark"}`}>
       <div className="profile__main-container">
@@ -12,22 +28,28 @@ function Profile({ isTheme }) {
           <h1 className="profile__name">Pavel Bondarevskii</h1>
           <p className="profile__job">Frontend разработчик</p>
           <div className="profile__contact">
-            <a href="https://t.me/@stupidhair" target="_blank" rel="noreferrer">
+            <a href="https://t.me/stupidhair" target="_blank" rel="noreferrer">
               <div
                 className={`contact-icon telegram ${
                   isTheme ? "telegram-light" : ""
                 }`}
               ></div>
             </a>
-            <a
-              href="https://gohre94@gmail.com"
-              target="_blank"
-              rel="noreferrer"
+
+            <div
+              onClick={copyEmail}
+              className={`contact-icon mail ${isTheme ? "mail-light" : ""}`}
             >
-              <div
-                className={`contact-icon mail ${isTheme ? "mail-light" : ""}`}
-              ></div>
-            </a>
+              <p
+                className={`copy__text ${
+                  isCopied ? "fade-in-out" : "text-hidden"
+                }`}
+              >
+                {" "}
+                Адрес скопирован!
+              </p>
+            </div>
+
             <a
               href="https://instagram.com/pavelbondarevskii"
               target="_blank"
